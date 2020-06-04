cb0630b06a4c168e76a0e71e4a550bf7e90d0356
avishith <pmshithu143@gmail.com>
2020-06-05 01:04:56 +0530

    Aboutus added

---
+++ app/src/main/java/com/hari/rideit/Controller/AboutusActivity.kt
@@ -0,0 +1,12 @@
+package com.hari.rideit.Controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hari.rideit.R
+
+class AboutusActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aboutus)
+    }
+}
