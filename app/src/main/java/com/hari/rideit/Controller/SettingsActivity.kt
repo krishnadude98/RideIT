9ac15b982e38f03007ff41427d4b319a9da36260
Vishnu Vinay <vishnuv1811@gmail.com>
2020-06-05 06:28:33 -0700

    Entho enn cheythu don't remember

--- app/src/main/java/com/hari/rideit/Controller/SettingsActivity.kt
+++ app/src/main/java/com/hari/rideit/Controller/SettingsActivity.kt
@@ -8,5 +8,9 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        fun accountSettingsOnClick () {
+
+        }
     }
 }
